2fef26ad9d82b5b6e8411bdbe38ea7dddc92cf0e
AudioEnhancer <115884653+ASR2099@users.noreply.github.com>
2022-10-19 23:19:58 +0800

    Add files via upload

---
+++ RIR-Generator/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import os.path
+
+extensions = [
+	Extension("pyrirgen", ["rirgen.cpp", "pyrirgen.pyx"],
+		include_dirs=[
+			os.path.abspath("."),
+		],
+		library_dirs=[os.path.abspath(".")],
+		libraries=["rirgen"],
+		language='c++'
+	),
+]
+
+setup(
+	name = 'pyrirgen',
+	ata_files = [('', ['librirgen.so'])],
+	ext_modules=cythonize(extensions, compiler_directives = {
+		'language_level': 3, # Python 3
+		'embedsignature': True, # add method signature to docstrings, thus tools can display it after compilation
+		'c_string_encoding': 'default',
+		'c_string_type': 'str',
+	}),
+)
